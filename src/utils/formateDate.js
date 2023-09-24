bf5522f6e82710fe44ff857161773a871be07d85
Michael Ojogbo <linkupxc@gmail.com>
2023-09-24 10:32:14 +0100

    First commit

---
+++ src/utils/formateDate.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const formateDate = (date, config) => {
+ 
+    const defaultOptions = {day:'numeric', month:'short', year:'numeric'}
+    const options = config ? config : defaultOptions
+
+    return new Date(date).toLocaleDateString('en-US', options);
+}
